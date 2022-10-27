db1f2ba87ab83b5e9f272abbe5014165e4f563b1
Anargyros Tsadimas <tsadimas@hua.gr>
2022-10-27 11:27:02 +0300

    initial commit

---
+++ src/main/java/gr/hua/dit/ds/HomeController.java
@@ -0,0 +1,14 @@
+package gr.hua.dit.ds;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String home() {
+		return "home";
+	}
+
+}
